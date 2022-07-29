2da7fcd614945346734ff981c969b60ff69a7de0
Patryk Betlinski <pbetlinski61@gmail.com>
2022-07-29 23:34:26 +0200

    Fix game function, add function that asks user for choice

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
+// Ask user for choice - Rock/paper/scissors
+function playerSelection(){
+let playersChoice = prompt('What is your choice?', '');
+return playersChoice.charAt(0).toUpperCase() + playersChoice.slice(1);
+}
 // Get computer's choice - Rock/paper/scissors
 let items = ['Rock', 'Paper', 'Scissors'];
 let computerSelection = getComputersChoice(items);
-let playersChoice = prompt('What is your choice?', '');
-let playerSelection = playersChoice.charAt(0).toUpperCase() + playersChoice.slice(1);
 
 function getComputersChoice(items) {
   return items[Math.floor(Math.random() * items.length)]
 }
+// Check who wins
 function gameOn(playerSelection, computerSelection) {
  if (playerSelection == computerSelection){
   return "It's a tie!"
  }
  else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock"){
-  return "Evil AI wins!"
+  return `Evil AI wins! ${computerSelection} beats ${playerSelection}!`
  }
 else {
-  return "Congratulations human! Keep fighting!"
+  return `Congratulations human! ${playerSelection} beats ${computerSelection}!`
 }
+}
+function game(){
+  for (let i = 0; i < 5; i++){
+
+  }
 }
 
-// Ask user for choice - Rock/paper/scissors
-// Check who wins
-// Print the outcome
-// Repeat until one of the players scores 5 points
-// Print the message to the winner
+
+
