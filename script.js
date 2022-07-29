4b44d0e074714160284eaea94723f74a2d03b030
Patryk Betlinski <pbetlinski61@gmail.com>
2022-07-29 23:19:50 +0200

    Add function for one round of rock paper scissors

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // Get computer's choice - Rock/paper/scissors
-let items = ['rock', 'paper', 'scissors'];
+let items = ['Rock', 'Paper', 'Scissors'];
+let computerSelection = getComputersChoice(items);
+let playersChoice = prompt('What is your choice?', '');
+let playerSelection = playersChoice.charAt(0).toUpperCase() + playersChoice.slice(1);
+
 function getComputersChoice(items) {
   return items[Math.floor(Math.random() * items.length)]
 }
-console.log(getComputersChoice(items));
+function gameOn(playerSelection, computerSelection) {
+ if (playerSelection == computerSelection){
+  return "It's a tie!"
+ }
+ else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock"){
+  return "Evil AI wins!"
+ }
+else {
+  return "Congratulations human! Keep fighting!"
+}
+}
+
 // Ask user for choice - Rock/paper/scissors
 // Check who wins
 // Print the outcome
